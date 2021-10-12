25954fd87a512b1edf1c4ced790c3dbf9fde4c57
Gagan patil <gaganpatil@Gagans-MacBook-Pro.local>
2021-10-12 19:22:19 +0530

    UI completed

---
+++ app/src/main/java/com/ather/a2048/view/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ather.a2048.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ather.a2048.R
+import com.ather.a2048.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding?.let {
+            it.unbind()
+            binding = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/ather/a2048/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.ather.a2048.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+}
